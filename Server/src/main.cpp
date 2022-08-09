c7280a00fdf5e11295bcfd66cd601b6b13999035
Toby-Dodge <toby.dodge@keyin.com>
2022-08-09 14:16:19 -0230

    Basic http server that returns "hello world"

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include "include/httplib.h"
+#define CROW_MAIN
+#include "../include/crow_all.h"
+#include <pthread.h>
 
 int main() {
 
+    crow::SimpleApp app;
+
+    //Perhaps we can get params in the link? and get the keys and what not via that
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
     std::cout << "Ay Yo?";
+
+    app.port(18080).multithreaded().run();
 
     return 0; 
 }
