f35b4d2ee213a57ee49e46d9f8b12fd7e14da41d
BeaconBrigade <beaconbrigade@gmail.com>
2022-08-10 10:55:57 -0230

    Added postgres

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -2,10 +2,11 @@
 #define CROW_MAIN
 #include "../include/crow_all.h"
 #include <pthread.h>
+#include <pqxx/pqxx>
 
 int main() {
-
     crow::SimpleApp app;
+    pqxx::connection c("dbname=server user=server");
 
     //Perhaps we can get params in the link? and get the keys and what not via that
     CROW_ROUTE(app, "/")([](){
