6d5a7e53d7e6b86d1965cf23c95b7ce1a549f374
Server <>
2022-08-10 14:47:38 -0230

    fixed migrations

--- Server/sql/passwords/up.sql
+++ Server/sql/passwords/up.sql
@@ -1,9 +1,9 @@
 -- password table
-CREATE TABLE Passwords {
+CREATE TABLE Passwords (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES Users(id),
     account_name TEXT NOT NULL,
     password TEXT NOT NULL,
-    salt TEXT NOT NULL,
-};
+    salt TEXT NOT NULL
+);
 
