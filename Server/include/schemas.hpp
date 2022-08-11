cf2da66b71a64a5b384c0b3874702835e50e91bb
BeaconBrigade <beaconbrigade@gmail.com>
2022-08-11 11:36:24 -0230

    fmt

--- Server/include/schemas.hpp
+++ Server/include/schemas.hpp
@@ -2,32 +2,31 @@
 
 #include <string>
 
-class User 
-{
+class User {
 public:
-    uint64_t m_id;
-    std::string m_username;
-    std::string m_password_hash;
-    std::string m_salt;
+  uint64_t m_id;
+  std::string m_username;
+  std::string m_password_hash;
+  std::string m_salt;
+
 public:
-    User(uint64_t id, std::string username, std::string password_hash, std::string salt)
-        : m_username(username), m_password_hash(password_hash), m_id(id), m_salt(salt)
-    {
-    }
+  User(uint64_t id, std::string username, std::string password_hash,
+       std::string salt)
+      : m_username(username), m_password_hash(password_hash), m_id(id),
+        m_salt(salt) {}
 };
 
-class Password
-{
+class Password {
 public:
-    uint64_t m_id;
-    uint64_t m_user_id;
-    std::string m_account_name;
-    std::string m_password;
-    std::string m_salt;
-public:
-    Password(uint64_t id, uint64_t user_id, std::string account_name, std::string password, std::string salt)
-        : m_id(id), m_user_id(user_id), m_account_name(account_name), m_password(password), m_salt(salt)
-    {
-    }
-};
+  uint64_t m_id;
+  uint64_t m_user_id;
+  std::string m_account_name;
+  std::string m_password;
+  std::string m_salt;
 
+public:
+  Password(uint64_t id, uint64_t user_id, std::string account_name,
+           std::string password, std::string salt)
+      : m_id(id), m_user_id(user_id), m_account_name(account_name),
+        m_password(password), m_salt(salt) {}
+};
