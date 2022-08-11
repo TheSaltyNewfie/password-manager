08946c31baf95f1432172a8e73af23091a2d3f08
rachael.moores <Rachael.Moores@keyin.com>
2022-08-11 15:26:47 -0230

    slay

--- Client/Window1.py
+++ Client/Window1.py
@@ -16,6 +16,12 @@ def closed_window():
 
 app = App(title="Company Name", layout="grid")
 
+sign_up_window = Window(app, title="Sign Up", layout="grid")
+sign_up_window.hide()
+
+Vault_window = Window(app, title="My Passwords", layout="grid")
+Vault_window.hide()
+
 ##  Login  ##
 
 User = Text(app, text="User ID: ", size=20, grid=[0, 0], align="left")
@@ -42,27 +48,15 @@ Sign_Up = PushButton(app, text="Sign Up", grid=[0, 8], width=5, height=2, align=
 
 ##  Window 2  ##
 
-sign_up_window = Window(app, title="Sign Up", layout="grid")
-sign_up_window.hide()
-
 New_User = Text(sign_up_window, text="User ID: ", size=20, grid=[0, 0], align="left")
 New_UserBox = TextBox(sign_up_window, grid=[1, 0], align="left", width="fill")
 
 New_Pass = Text(sign_up_window, text="Master Password: ", grid=[0, 1], size=20, align="left")
 New_PassBox = TextBox(sign_up_window, align="left", grid=[1, 1], width="fill")
 
-Password_Length = Text(sign_up_window, text="Password Strength", grid=[0, 2], align="left")
-
-# Checker = Password_Length.value
-# def password_check(Checker):
-
 
 Confirm_Pass = Text(sign_up_window, text="Confirm Password: ", grid=[0, 3], size=20, align="left")
 Confirm_PassBox = TextBox(sign_up_window, align="left", grid=[1, 3], width="fill")
-
-
-Vault_window = Window(app, title="My Passwords", layout="grid")
-Vault_window.hide()
 
 
 def confirm_button():
@@ -82,12 +76,11 @@ def confirm_button():
         Confirm.when_clicked = no_username()
     elif len(New_PassBox.value) < 12 or len(Confirm_PassBox.value) < 12:
         Confirm.when_clicked = too_short()
-
     else:
-        if New_PassBox.value == Confirm_PassBox.value:
-            Confirm.when_clicked = correct_password()
-        elif New_PassBox.value != Confirm_PassBox.value:
+        if New_PassBox.value != Confirm_PassBox.value:
             Confirm.when_clicked = match()
+        elif New_PassBox.value == Confirm_PassBox.value:
+            Confirm.when_clicked = correct_password()
 
 
 Back = PushButton(sign_up_window, text="Back", align="bottom", grid=[2,0], command=closed_window)
@@ -96,5 +89,15 @@ Confirm = PushButton(sign_up_window, text="Confirm", align="right", grid=[2,1],c
 
 ##  Window 3  ##
 
+def New_Password():
+    Add_Password.show()
+
+Add_Password = Window(app, title="New Password", layout="grid")
+Add_Password.hide()
+
+Blank_Button = PushButton(Vault_window, text="    Saved Passwords     ", grid=[0, 0], width=40)
+Plus_password = PushButton(Vault_window, text="+", grid=[5, 0])
+
+Plus_password.when_clicked = New_Password()
 
 app.display()
