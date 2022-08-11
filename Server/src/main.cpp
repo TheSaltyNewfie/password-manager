227de592e7c3fff86e47bd809af628be8ce40668
BeaconBrigade <beaconbrigade@gmail.com>
2022-08-11 11:36:24 -0230

    fmt

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -2,49 +2,50 @@
 #define CROW_MAIN
 #include "../include/crow_all.h"
 #include "../include/schemas.hpp"
-#include <pthread.h>
 #include <pqxx/pqxx>
+#include <pthread.h>
 
 int main() {
-    crow::SimpleApp app;
-    pqxx::connection c("dbname=server user=server");
+  crow::SimpleApp app;
+  pqxx::connection c("dbname=server user=server");
 
-    CROW_ROUTE(app, "/auth").methods(crow::HTTPMethod::GET, crow::HTTPMethod::PATCH)
-    ([](const crow::request& req)
-    {
+  CROW_ROUTE(app, "/auth")
+      .methods(crow::HTTPMethod::GET,
+               crow::HTTPMethod::PATCH)([](const crow::request &req) {
+        char *id = req.url_params.get("id");
+        char *username = req.url_params.get("username");
+        char *password_hash = req.url_params.get("password_hash");
+        char *salt = req.url_params.get("salt");
 
-        char* id = req.url_params.get("id");
-        char* username = req.url_params.get("username");
-        char* password_hash = req.url_params.get("password_hash");
-        char* salt = req.url_params.get("salt");
+        if (id != nullptr && username != nullptr && password_hash != nullptr &&
+            salt != nullptr) {
+          // std::cout << "[AUTH] USER ID: " << user_id << "\nPASSWORD: " <<
+          // password << std::endl;
 
-        if(id != nullptr && username != nullptr && password_hash != nullptr && salt != nullptr)
-        {
-            //std::cout << "[AUTH] USER ID: " << user_id << "\nPASSWORD: " << password << std::endl;
-
-            return crow::json::wvalue{{"id", id}, {"username", username}, {"password_hash", password_hash}, {"salt", salt}};
+          return crow::json::wvalue{{"id", id},
+                                    {"username", username},
+                                    {"password_hash", password_hash},
+                                    {"salt", salt}};
         }
 
         return crow::json::wvalue{{"error", "error details would be here"}};
-    });
+      });
 
-    CROW_ROUTE(app, "/create").methods(crow::HTTPMethod::GET, crow::HTTPMethod::PATCH)
-    ([](){
+  CROW_ROUTE(app, "/create")
+      .methods(crow::HTTPMethod::GET, crow::HTTPMethod::PATCH)([]() {
+        return "You technically created an account, but not really cause this "
+               "doesnt work yet :)";
+      });
 
-        return "You technically created an account, but not really cause this doesnt work yet :)";
+  CROW_ROUTE(app, "/login")
+      .methods(crow::HTTPMethod::GET, crow::HTTPMethod::PATCH)([]() {
+        return "You technically logged into an account, but not really cause "
+               "this doesnt work yet :)";
+      });
 
-    });
+  std::cout << "Ay Yo?\n";
 
-    CROW_ROUTE(app, "/login").methods(crow::HTTPMethod::GET, crow::HTTPMethod::PATCH)
-    ([](){
+  app.port(18080).multithreaded().run();
 
-        return "You technically logged into an account, but not really cause this doesnt work yet :)";
-
-    });
-
-    std::cout << "Ay Yo?\n";
-
-    app.port(18080).multithreaded().run();
-
-    return 0; 
+  return 0;
 }
