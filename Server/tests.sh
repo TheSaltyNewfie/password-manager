c9318a13f463afbf4076ed9359055f48cf0b18b0
BeaconBrigade <beaconbrigade@gmail.com>
2022-08-12 09:22:50 -0230

    Updated test to be more readable

--- Server/tests.sh
+++ Server/tests.sh
@@ -3,17 +3,17 @@
 echo "Start server before using this"
 printf "Run ./build/main&\n\n"
 
-echo "Should work, creating user."
+printf "Should work, creating user."
 curl 'localhost:18080/create?username=joe&password_hash=blargblarg'
 
-echo "Should error, duplicate key."
+printf "\n\nShould error, duplicate key."
 curl 'localhost:18080/create?username=joe&password_hash=blargblarg'
 
-echo "Should work, token"
+printf "\n\nShould work, token"
 curl 'localhost:18080/auth?username=joe&password_hash=blargblarg'
 
-echo "Should fail, user doesn't exist (wrong username)"
+printf "\n\nShould fail, user doesn't exist (wrong username)"
 curl 'localhost:18080/auth?username=joey&password_hash=blargblarg'
 
-echo "Should fail, user doesn't exist (wrong password)"
+printf "\n\nShould fail, user doesn't exist (wrong password)"
 curl 'localhost:18080/auth?username=joe&password_hash=blargblar'
