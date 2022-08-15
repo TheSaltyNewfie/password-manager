ae3a74e7a97974af66b1bb4e2eb02cc3cb036eee
Toby-Dodge <toby.dodge@keyin.com>
2022-08-15 14:51:30 -0230

    new file:   Server/include/encryption.h 	new file:   Server/src/encryption.cpp

---
+++ Server/include/gui_handling.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+//#include <../imgui_src/imgui.h>
+
+//void draw_menu(int version_number);
---
+++ Server/src/gui_handling.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*
+void draw_menu(int version_number){
+    ImGui::Text("Admin Menu %d", version_number);
+
+    if(ImGui::Button("Start"))
+        std::cout << "This would start the server, but its not ready";
+    if(ImGui::Button("Halt"))
+        std::cout << "This would halt the server, but its not ready";
+
+}*/
