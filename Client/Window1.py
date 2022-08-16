ec92ad5139e0a61554723ba9b54abf37b10edf71
hannah.cameron <hannah.cameron@keyin.com>
2022-08-16 15:59:59 -0230

    we didn't do it it

--- Client/Window1.py
+++ Client/Window1.py
@@ -1,7 +1,13 @@
-from guizero import App, Text, TextBox, PushButton, Window, error
+from guizero import App, Text, TextBox, PushButton, Window, Box
 import random
+import api
 
 ##  Open and close windows  ##
+
+token = ""
+passwords = []
+username = ""
+password = ""
 
 
 def open_window():
@@ -9,6 +15,7 @@ def open_window():
 
 
 def open_Vault():
+    show_passwords()
     Window.show(Vault_window)
 
 
@@ -29,50 +36,91 @@ def logging_off():
     Window.hide(sign_up_window)
 
 
+def signup():
+    global token, username, password
+    response = api.create(New_UserBox.value, New_PassBox.value)
+    if "error" in response:
+        app.warn("Error", response["error"])
+        return
+    response = api.login(New_UserBox.value, New_PassBox.value)
+    if "error" in response:
+        app.warn("Error", response["error"])
+        return
+    token = response["token"]
+    username = New_UserBox.value
+    password = New_PassBox.value
+    open_Vault()
+
+
+def login():
+    global token, passwords, username, password
+    response = api.login(User_Box.value, Pass_Box.value)
+    if "error" in response:
+        app.warn("Error", response["error"])
+        return
+    token = response["token"]
+    response = api.get_passwords(User_Box.value, Pass_Box.value, token)
+    if "error" in response:
+        app.warn("Error", response["error"])
+        return
+    passwords = response
+    username = User_Box.value
+    password = Pass_Box.value
+    open_Vault()
+
+
 ##  Window 1  ##
 
-app = App(title="Company Name", layout="grid")
+app = App(title="Company Name", layout="grid", bg="sky blue")
 
-sign_up_window = Window(app, title="Sign Up", layout="grid")
-sign_up_window.hide()
+sign_up_window = Window(app, title="Sign Up", layout="grid", bg ="MistyRose3")
 
-Vault_window = Window(app, title="My Passwords", layout="grid")
-Vault_window.hide()
 
-Password_window = Window(app, title="New Password", layout="grid")
-Password_window.hide()
+Vault_window = Window(app, title="My Passwords", layout="grid", bg="MistyRose1")
+
+
+Password_window = Window(app, title="New Password", layout="grid", bg ="sky blue")
+
 
 ##  Login  ##
 
-User = Text(app, text="User ID: ", size=20, grid=[0, 0], align="left")
-User_Box = TextBox(app, grid=[1, 0], align="left", width="fill")
+User = Text(app, text="User ID: ", size=20, grid=[0, 0], align="left", bg="white")
+User_Box = TextBox(app, grid=[1, 0], align="left", width="fill", )
+User_Box.bg = "DarkOrange1"
+User_Box.text_color ="blue"
 
-Master_Pass = Text(app, text="Master Password: ", grid=[0, 1], size=20, align="left")
+Master_Pass = Text(app, text="Master Password: ", grid=[0, 1], size=20, align="left", bg="white")
 Pass_Box = TextBox(app, align="left", grid=[1, 1], width="fill", hide_text=True)
+Pass_Box.bg = "DarkOrange1"
 
-Login = PushButton(app, text="Login", grid=[0, 2], align="left", width=5, height=2)
-
+Login = PushButton(app, text="Login", grid=[0, 2], align="left", width=5, height=2, command=login)
 
 Blank = Text(app, text="        ", grid=[0, 5], align="left", size=20)
 
 
 ##  Create Account  ##
 
-Welcome = Text(app, text="New to Company? Welcome, sign up below:", grid=[0, 6], align="left", size=15)
+Welcome = Text(app, text="New to Company? Welcome, sign up below:", grid=[0, 6], align="left", size=15, font= "Comic Sans MS")
 Sign_Up = PushButton(app, text="Sign Up", grid=[0, 8], width=5, height=2, align="left", command=open_window)
 
 
 ##  Window 2  ##
 
-New_User = Text(sign_up_window, text="User ID: ", size=20, grid=[0, 0], align="left")
+New_User = Text(sign_up_window, text="User ID: ", size=20, grid=[0, 0], align="left", bg='white')
 New_UserBox = TextBox(sign_up_window, grid=[1, 0], align="left", width="fill")
+New_User.text_color = "red3"
+New_UserBox.bg = "coral2"
 
-New_Pass = Text(sign_up_window, text="Master Password: ", grid=[0, 1], size=20, align="left")
+New_Pass = Text(sign_up_window, text="Master Password: ", grid=[0, 1], size=20, align="left", bg="white")
 New_PassBox = TextBox(sign_up_window, align="left", grid=[1, 1], width="fill", hide_text=True)
+New_Pass.text_color = "red3"
+New_PassBox.bg = "coral2"
 
 
-Confirm_Pass = Text(sign_up_window, text="Confirm Password: ", grid=[0, 3], size=20, align="left")
+Confirm_Pass = Text(sign_up_window, text="Confirm Password: ", grid=[0, 3], size=20, align="left", bg="white")
 Confirm_PassBox = TextBox(sign_up_window, align="left", grid=[1, 3], width="fill", hide_text=True)
+Confirm_Pass.text_color = "red3"
+Confirm_PassBox.bg = 'coral2'
 
 
 def confirm_button():
@@ -86,7 +134,7 @@ def confirm_button():
         sign_up_window.warn("Uh oh!", "Please enter a password that is 12 characters.")
 
     def correct_password():
-        open_Vault()
+        signup()
 
     if (len(New_UserBox.value) == 0 or len(New_PassBox.value) == 0 or len(Confirm_PassBox.value) == 0):
         Confirm.when_clicked = no_username
@@ -105,29 +153,54 @@ Confirm = PushButton(sign_up_window, text="Confirm", align="right", grid=[2, 1],
 
 ##  Window 3  ##
 
+def show_passwords():
+    print(passwords)
+    for passwd in passwords[0]:
+        Text(List_of_passwords, f"Title: {passwd['title']}, Username: {passwd['username']}, Password: {passwd['password']}")
+
+
 Blank_Button = PushButton(Vault_window, text="    Saved Passwords     ", grid=[0, 0], width=40)
 Plus_password = PushButton(Vault_window, text="+", grid=[5, 0], command=open_Password)
+List_of_passwords = Box(Vault_window, grid=[0, 1])
 
 Logout = PushButton(Vault_window, text="Log Out", grid=[5, 5], command=logging_off)
 
-Plus_password.when_clicked = open_Password()
+Plus_password.when_clicked = open_Password
 
 
 ##  Window 4  ##
 
+def save_password():
+    global token, username, password, passwords
+    response = api.new_password(username, password, token, Username_TextBox.value, Password_TextBox.value)
+    if "error" in response:
+        app.warn("Error", response["error"])
+        return
+    app.info("Success!!!!!!!!!!", "You've successfully created a new password!")
+    response = api.get_passwords(User_Box.value, Pass_Box.value, token)
+    if "error" in response:
+        app.warn("Error", response["error"])
+        return
+    passwords = response
+
+
 Password_Name = Text(Password_window, text="Title: ", size=20, grid=[0, 0], align="left")
 Name_TextBox = TextBox(Password_window, grid=[1, 0], width="fill", align="left")
+Name_TextBox.bg = "DarkOrange1"
+
 
 Password_Username = Text(Password_window, text="Username/Email: ", size=20, grid=[0, 1], align="left")
 Username_TextBox = TextBox(Password_window, grid=[1, 1], width="fill", align="left")
+Username_TextBox.bg = "DarkOrange1"
 
 Password_Pass = Text(Password_window, text="Password: ", size=20, grid=[0, 2], align="left")
 Password_TextBox = TextBox(Password_window, grid=[1, 2], width="fill", align="left")
+Password_TextBox.bg = "DarkOrange1"
 
 Password_Blank = Text(Password_window, text="                  ", size=20, grid=[0, 3], width="fill")
 
 Exit_Button = PushButton(Password_window, text="Back", height=1, width=2, grid=[0, 8], align="left", command=close_Password)
-Save_Button = PushButton(Password_window, text="Save", height=1, width=2, grid=[1, 8])
+Save_Button = PushButton(Password_window, text="Save", height=1, width=2, grid=[1, 8], command=save_password)
 
 Password_Text = Text(Password_window, text="To generate a password: ", size=15, grid=[0, 4], align="left")
 Password_Cont = Text(Password_window, text="Enter preferred number of characters.", size=15, grid=[0, 5], align="left")
@@ -135,17 +208,20 @@ Password_Length = TextBox(Password_window, grid=[1, 5], align="left", width=2, h
 
 
 def get_passwords():
-    password_possibilities = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,./?!@#$%&(){}1234567890")
+    password_possibilities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,./?!@#$%&(){}1234567890"
 
     length = int(Password_Length.value)
-
+    password = ""
     for c in range(length):
-        password = ""
         password += random.choice(password_possibilities)
         password = str(password)
         printpass = TextBox(Password_window, text=password, width=20, height=10, grid=[0, 10], visible=True, multiline=True)
+        printpass.bg = "white"
 
 
 Generate = PushButton(Password_window, command=get_passwords, text="Generate", grid=[1, 7])
 
+Vault_window.hide()
+sign_up_window.hide()
+Password_window.hide()
 app.display()
--- Client/api.py
+++ Client/api.py
@@ -3,41 +3,41 @@ import requests
 
 def create(username, password_hash):
     response = requests.get(
-        f"http://keyin.thesaltynewfie.ca/create?username={username}&password_hash={password_hash}"
+        f"https://keyin.thesaltynewfie.ca/create?username={username}&password_hash={password_hash}"
     )
     return response.json()
 
 
 def login(username, password_hash):
     response = requests.get(
-        f"http://keyin.thesaltynewfie.ca/auth?username={username}&password_hash={password_hash}"
+        f"https://keyin.thesaltynewfie.ca/auth?username={username}&password_hash={password_hash}"
     )
     return response.json()
 
 
 def delete_user(username, password_hash):
     response = requests.get(
-        f"http://keyin.thesaltynewfie.ca/delete?username={username}&password_hash={password_hash}"
+        f"https://keyin.thesaltynewfie.ca/delete?username={username}&password_hash={password_hash}"
     )
     return response.json()
 
 
-def new_password(username, password_hash, token, account_username, account_password):
+def new_password(username, password_hash, token, title, account_username, account_password):
     response = requests.get(
-        f"http://keyin.thesaltynewfie.ca/new-password?username={username}&password_hash={password_hash}&token={token}&pass_user={account_username}&pass_pass={account_password}"
+        f"https://keyin.thesaltynewfie.ca/new-password?username={username}&password_hash={password_hash}&token={token}&title={title}&pass_user={account_username}&pass_pass={account_password}"
     )
     return response.json()
 
 
 def get_passwords(username, password_hash, token):
     response = requests.get(
-        f"http://keyin.thesaltynewfie.ca/get-passwords?username={username}&password_hash={password_hash}&token={token}"
+        f"https://keyin.thesaltynewfie.ca/get-passwords?username={username}&password_hash={password_hash}&token={token}"
     )
     return response.json()
 
 
 def delete_password(username, password_hash, token, account_username, account_password):
     response = requests.get(
-        f"http://keyin.thesaltynewfie.ca/new-password?username={username}&password_hash={password_hash}&token={token}&pass_user={account_username}&pass_pass={account_password}"
+        f"https://keyin.thesaltynewfie.ca/new-password?username={username}&password_hash={password_hash}&token={token}&pass_user={account_username}&pass_pass={account_password}"
     )
     return response.json()
