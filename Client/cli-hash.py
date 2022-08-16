62aea4765b9d3af26c6040f0894fc0e994ebf1d9
BeaconBrigade <beaconbrigade@gmail.com>
2022-08-16 10:19:48 -0230

    Fixed cli app

--- Client/cli-hash.py
+++ Client/cli-hash.py
@@ -16,13 +16,15 @@ def prf(key, msg):
 
 if command == "help":
     print(f"using {sys.argv[0]}")
-    print(f"{sys.argv[0]} aes encrypt|decrypt <master_key>")
+    print(
+        f"{sys.argv[0]} aes encrypt|decrypt <stretched_master_key> <password_to_hash> <16_byte_init_vec>"
+    )
     print(f"{sys.argv[0]} scrypt <password> <salt>")
 elif command == "aes":
     if sys.argv[2] == "encrypt":
-        print(AES(sys.argv[3]).encrypt_cbc())
+        print(AES(sys.argv[3]).encrypt_cbc(sys.argv[4], sys.argv[4]))
     elif sys.argv[2] == "decrypt":
-        print(AES(sys.argv[3]).decrypt_cbc())
+        print(AES(sys.argv[3]).decrypt_cbc(sys.argv[4], sys.argv[5]))
     else:
         print("what the ****!!")
 elif command == "scrypt":
@@ -35,7 +37,7 @@ elif command == "scrypt":
             p=1,
             dkLen=128,
             prf=prf,
-        )
+        ).scrypt()
     )
 else:
     print("What the ****!!")
