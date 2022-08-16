f65cc72e596499069c6133a2f961b52cd4dc656d
hannah.cameron <hannah.cameron@keyin.com>
2022-08-16 13:44:07 -0230

    we did it

--- Client/api.py
+++ Client/api.py
@@ -3,41 +3,41 @@ import requests
 
 def create(username, password_hash):
     response = requests.get(
-        f"https://keyin.thesaltynewfie.ca/create?username={username}&password_hash={password_hash}"
+        f"http://keyin.thesaltynewfie.ca/create?username={username}&password_hash={password_hash}"
     )
-    return respone.json()
+    return response.json()
 
 
 def login(username, password_hash):
     response = requests.get(
-        f"https://keyin.thesaltynewfie.ca/auth?username={username}&password_hash={password_hash}"
+        f"http://keyin.thesaltynewfie.ca/auth?username={username}&password_hash={password_hash}"
     )
     return response.json()
 
 
 def delete_user(username, password_hash):
     response = requests.get(
-        f"http://127.0.0.1:18080/delete?username={username}&password_hash={password_hash}"
+        f"http://keyin.thesaltynewfie.ca/delete?username={username}&password_hash={password_hash}"
     )
     return response.json()
 
 
 def new_password(username, password_hash, token, account_username, account_password):
     response = requests.get(
-        f"http://127.0.0.1:18080/new-password?username={username}&password_hash={password_hash}&token={token}&pass_user={account_username}&pass_pass={account_password}"
+        f"http://keyin.thesaltynewfie.ca/new-password?username={username}&password_hash={password_hash}&token={token}&pass_user={account_username}&pass_pass={account_password}"
     )
     return response.json()
 
 
 def get_passwords(username, password_hash, token):
     response = requests.get(
-        f"http://127.0.0.1:18080/get-passwords?username={username}&password_hash={password_hash}&token={token}"
+        f"http://keyin.thesaltynewfie.ca/get-passwords?username={username}&password_hash={password_hash}&token={token}"
     )
     return response.json()
 
 
 def delete_password(username, password_hash, token, account_username, account_password):
     response = requests.get(
-        f"http://127.0.0.1:18080/new-password?username={username}&password_hash={password_hash}&token={token}&pass_user={account_username}&pass_pass={account_password}"
+        f"http://keyin.thesaltynewfie.ca/new-password?username={username}&password_hash={password_hash}&token={token}&pass_user={account_username}&pass_pass={account_password}"
     )
     return response.json()
